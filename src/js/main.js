c1bbda96378a88047a1fb5a7eda2c4de5559c5fe
Dorenay <dorenay@gmail.com>
2019-06-20 12:13:01 +0300

    first build

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+
+//= ../../bower_components/slick-carousel/slick/slick.js
+//= ../../bower_components/bootstrap/dist/js/bootstrap.min.js
+//= ../../bower_components/select2/dist/js/select2.js
+
+
+/*
+ * Custom
+ */
+//= partials/app.js
